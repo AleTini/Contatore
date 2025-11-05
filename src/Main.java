1e60edcea8c70661baad7a4616092f9a5beceb76
AleTini <pizzy.tini.007@gmail.com>
2025-11-05 11:40:38 +0100

    Inserimento classe Contatore_Synchronized e adattamento classe Main seguendo i due tipi di classe contatore.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         Contatore counter = new Contatore();
+        Contatore_Synchronized syncCounter = new Contatore_Synchronized();
 
         // Create multiple th   reads to increment the counter
         Thread t1 = new Thread(() -> counter.increment());
         Thread t2 = new Thread(() -> counter.increment());
+        Thread t3 = new Thread(() -> syncCounter.increment());
+        Thread t4 = new Thread(() -> syncCounter.increment());
 
         t1.start();
         t2.start();
+        t3.start();
+        t4.start();
 
         try {
             t1.join();
             t2.join();
+            t3.join();
+            t4.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         System.out.println("Final count: " + counter.getCount());
+        System.out.println("Final count: " + syncCounter.getCount());
     }
 }
